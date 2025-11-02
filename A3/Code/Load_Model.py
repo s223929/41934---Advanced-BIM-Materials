9f0a952d5419944909e8d377679662832ad96504
OlineMervig <s223898@dtu.dk>
2025-11-02 19:51:40 +0100

    Update Load_Model.py

--- A3/Code/Load_Model.py
+++ A3/Code/Load_Model.py
@@ -1,7 +1,7 @@
 import ifcopenshell
 from tkinter import Tk, filedialog
 
-# === Function to let the user select an IFC file ===
+# The user can select an IFC file
 def select_ifc_file():
     # Hide the main tkinter window
     root = Tk()
@@ -13,12 +13,13 @@ def select_ifc_file():
         filetypes=[("IFC files", "*.ifc"), ("All files", "*.*")]
     )
 
+    # Output, if the IFC file is not found
     if not file_path:
         raise FileNotFoundError("No IFC file selected. Please run again and choose a file.")
 
     print(f"\n IFC file selected:\n{file_path}\n")
     return file_path
 
-# === Load and store the IFC file as a reusable variable ===
+# The loaded IFC file can now be loaded into another part of the code
 ifc_path = select_ifc_file()
 ifc_file = ifcopenshell.open(ifc_path)
