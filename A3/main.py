ff10d60c6d90a0317313d4246c7e6cb2413506c7
s223929 <s223929@dtu.dk>
2025-11-10 15:47:54 +0100

    Create main.py

---
+++ A3/main.py
@@ -0,0 +1,12 @@
+import GWP_comparison
+
+
+def main():
+    """Entry point of the program."""
+    print("=== Starting GWP Comparison Program ===\n")
+    GWP_comparison.main()
+    print("\n=== Program finished successfully ===")
+
+
+if __name__ == "__main__":
+    main()
